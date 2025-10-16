b1d3734a8fd1d8410b98693b77c81a3295121e3f
Jade Meskill <jade.meskill@gmail.com>
2025-10-16 12:15:31 -0700

    get void and gap rpi3 version working

--- nix-config/hosts/void/configuration.nix
+++ nix-config/hosts/void/configuration.nix
@@ -16,6 +16,41 @@
 
   networking.hostName = "void"; # Define your hostname.
 
+  services.blocky = {
+    enable = true;
+    settings = {
+      ports.dns = 53; # Port for incoming DNS Queries.
+      upstreams.groups.default = [
+        "https://one.one.one.one/dns-query" # Using Cloudflare's DNS over HTTPS server for resolving queries.
+      ];
+      # For initially solving DoH/DoT Requests when no system Resolver is available.
+      bootstrapDns = {
+        upstream = "https://one.one.one.one/dns-query";
+        ips = ["1.1.1.1" "1.0.0.1"];
+      };
+      #Enable Blocking of certain domains.
+      blocking = {
+        denylists = {
+          #Adblocking
+          ads = ["https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"];
+          #Another filter for blocking adult sites
+          adult = ["https://blocklistproject.github.io/Lists/porn.txt"];
+          #You can add additional categories
+        };
+        #Configure what block categories are used
+        clientGroupsBlock = {
+          default = ["ads"];
+          kids-ipad = ["ads" "adult"];
+        };
+      };
+      caching = {
+        minTime = "5m";
+        maxTime = "30m";
+        prefetching = true;
+      };
+    };
+  };
+
   services.keepalived = {
     enable = true;
 
@@ -31,6 +66,7 @@
       virtualRouterId = 35;
       useVmac = true;
       priority = 44;
+      noPreempt = true;
       virtualIps = [{addr = "10.55.10.34/24";}];
       # trackScripts = ["track_homepage"];
     };
