df3b3d26f10cec7dacee9972342e708a16a00e6e
Jade Meskill <jade.meskill@gmail.com>
2025-10-26 12:31:12 +0100

    add builders to darwin, update flake-checker

--- nix-config/modules/darwin/default/config.nix
+++ nix-config/modules/darwin/default/config.nix
@@ -16,6 +16,14 @@
   # direnv configuration
   programs.direnv.enable = lib.mkDefault true;
   nix.settings.trusted-users = ["root" "@admin" "jmeskill"];
+  nix.settings.experimental-features = ["external-builders"];
+  nix.settings.external-builders = [
+    {
+      "systems" = ["aarch64-linux" "x86_64-linux"];
+      "program" = "/usr/local/bin/determinate-nixd";
+      "args" = ["builder"];
+    }
+  ];
 
   # Add ability to use TouchID for sudo
   security.pam.services.sudo_local.touchIdAuth = true;
