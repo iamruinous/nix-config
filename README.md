14b05ef491b909885f5f9198f68e7a087ff58ce0
Jade Meskill <jade.meskill@gmail.com>
2025-11-02 14:30:07 -0700

    clean up readme, simplify ssh keys in messytty

--- README.md
+++ README.md
@@ -1,30 +1,4 @@
-# iamruinous dotfiles
-
-My personal dotfiles, managed with [chezmoi](https://www.chezmoi.io/) and [Nix](https://nixos.org/).
-
-## Installation
-
-### Prerequisites
-
-- [Git](https://git-scm.com/)
-- [chezmoi](https://www.chezmoi.io/install/)
-
-### Quick Start
-
-```sh
-chezmoi init iamruinous
-```
-
-This command will clone the repository. To see what changes will be made to your home directory, you can run:
-
-```sh
-chezmoi diff
-```
-
-To apply the changes, run:
-```sh
-chezmoi apply
-```
+# iamruinous nix-config
 
 ## Nix Configuration
 
--- hosts/obelisk/microvms/messytty.nix
+++ hosts/obelisk/microvms/messytty.nix
@@ -1,4 +1,9 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  sshKeys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL8rjXP/sjewv6kM1aTtNWkVZKJpZvIAXIRqL81IyEsm iamruinous@ruinous.social"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIHrfqbC3NHGjwLhV4qoBCWZ44DU7dfyhUJJ83XCD1LD messy@messytty"
+  ];
+in {
   # The configuration for the MicroVM.
   # Multiple definitions will be merged as expected.
   system.stateVersion = "25.05";
@@ -6,20 +11,14 @@
   networking.hostName = "messytty";
 
   users.users.root.password = "password";
-  users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL8rjXP/sjewv6kM1aTtNWkVZKJpZvIAXIRqL81IyEsm iamruinous@ruinous.social"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIHrfqbC3NHGjwLhV4qoBCWZ44DU7dfyhUJJ83XCD1LD messy@messytty"
-  ];
+  users.users.root.openssh.authorizedKeys.keys = sshKeys;
 
   users.users.messy = {
     password = "password";
     extraGroups = ["wheel"];
     group = "users";
     isNormalUser = true;
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL8rjXP/sjewv6kM1aTtNWkVZKJpZvIAXIRqL81IyEsm iamruinous@ruinous.social"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIHrfqbC3NHGjwLhV4qoBCWZ44DU7dfyhUJJ83XCD1LD messy@messytty"
-    ];
+    openssh.authorizedKeys.keys = sshKeys;
   };
 
   microvm = {
@@ -35,12 +34,6 @@
     ];
 
     interfaces = [
-      # {
-      #   type = "macvtap";
-      #   link = "enp2s0";
-      #   id = "mv-eth0";
-      #   mac = "02:00:00:00:00:01";
-      # }
       {
         type = "user";
         id = "vm-eth0";
@@ -64,24 +57,10 @@
 
   services.openssh = {
     enable = true;
-    # startWhenNeeded = true;
-    # listenAddresses = [];
-    # settings = {
-    #   PermitRootLogin = "yes";
-    #   PasswordAuthentication = true;
-    #   PermitEmptyPasswords = true;
-    #   UsePAM = false;
-    # };
   };
 
   environment.systemPackages = with pkgs; [
+    gemini
     tmux
   ];
-  # systemd.sockets.sshd = {
-  #   socketConfig = {
-  #     ListenStream = [
-  #       "vsock:1337:22"
-  #     ];
-  #   };
-  # };
 }
