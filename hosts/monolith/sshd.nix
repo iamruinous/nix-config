cad87bd06939c74b7fc3f61a2f1d440d0557b395
Jade Meskill <jade.meskill@gmail.com>
2025-11-10 00:15:45 -0700

    better override for messy key

--- hosts/monolith/sshd.nix
+++ hosts/monolith/sshd.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   environment.systemPackages = [
     pkgs.forgejo-shell
     pkgs.messy-restricted-shell
@@ -7,8 +11,8 @@
   users.users.git.shell = pkgs.forgejo-shell;
   users.users.git.extraGroups = ["docker"];
 
-  # users.users.messy.shell = pkgs.messy-restricted-shell;
   users.users.messy.extraGroups = ["docker"];
+  users.users.messy.openssh.authorizedKeys.keyFiles = lib.mkOverride 50 [];
   users.users.messy.openssh.authorizedKeys.keys = [
     "no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty,command=\"${pkgs.messy-restricted-shell}/bin/messy-restricted-shell\" ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIHrfqbC3NHGjwLhV4qoBCWZ44DU7dfyhUJJ83XCD1LD  messy@messy-tty"
   ];
