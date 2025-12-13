68ff3f95c082d83834b27575708de976a2a869cc
Jade Meskill <jade.meskill@gmail.com>
2025-12-12 22:55:32 -0700

    add zenith

---
+++ hosts/zenith/users/jmeskill/home-configuration.nix
@@ -0,0 +1,17 @@
+{
+  flake,
+  pkgs,
+  ...
+}: {
+  imports = [
+    flake.homeModules.default
+  ];
+
+  ruinous.openssh.tmux.attach.enable = true;
+
+  # systemd.user.tmpfiles.rules = [
+  #   "L+    /home/jmeskill/.local/bin/op-ssh-sign -    -    -     - ${pkgs.openssh}/bin/ssh-keygen"
+  # ];
+
+  home.stateVersion = "26.05";
+}
