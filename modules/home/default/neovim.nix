878ddd75f1930c728481d395e5131669e4813be5
Jade Meskill <iamruinous@ruinous.social>
2026-01-02 02:14:01 -0700

    feat(neovim): configure neovim program settings

--- modules/home/default/neovim.nix
+++ modules/home/default/neovim.nix
@@ -5,6 +5,18 @@
 }: let
   nvim_config = ../../../files/configs/nvim;
 in {
+  # Neovim text editor configuration
+  programs.neovim = {
+    enable = lib.mkDefault true;
+    package = pkgs.neovim-unwrapped;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    withNodeJs = true;
+    withPython3 = true;
+    withRuby = true;
+  };
+
   xdg.configFile = {
     "nvim" = {
       source = "${nvim_config}";
